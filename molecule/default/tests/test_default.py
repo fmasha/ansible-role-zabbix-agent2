fbc0751e88794dc24a3f5a472b9523d0642be6f2
Мария Лукашева <lukashevamv@ott.nsc.ru>
2023-12-24 18:16:06 +0300

    Add files

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,32 @@
+"""Role testing files using testinfra."""
+import os
+import pytest
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+def test_hosts_file(host):
+    """Validate /etc/hosts file."""
+    f = host.file("/etc/hosts")
+
+    assert f.exists
+    assert f.user == "root"
+    assert f.group == "root"
+
+@pytest.mark.parametrize('pkg', [
+  'zabbix-agent2',
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
+
+    assert package.is_installed
+
+@pytest.mark.parametrize('svc', [
+  'zabbix-agent2',
+])
+def test_svc(host, svc):
+    service = host.service(svc)
+
+    assert service.is_running
+    assert service.is_enabled
